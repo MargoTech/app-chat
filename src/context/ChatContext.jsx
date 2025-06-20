f8ef0e07968b9f0601328fac4ac1511249c58f81
MargoTech <margobartashevich@gmail.com>
2025-06-20 21:02:32 +0000

    Creating ChatContext.jsx

---
+++ src/context/ChatContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { fakeReply } from "../utils/mockSocket";
+
+const ChatContext = createContext();
+
+export function  ChatProvider({ children } {
+    const [messages, setMessages] = useState([]);
+    const username = localStorage.getItem("username");
+
+    const sendMessage = (text) => {
+        const msg = {
+            id: crypto.randomUUID(),
+            sender: username,
+            text,
+            timestamp: new Date().toISOString(),
+        };
+
+        setMessages((prev) => [...prev, msg]);
+
+        fakeReply(msg.text).then((reply) => {
+            setMessages((prev) => [...prev, reply]);
+        });
+    };
+
+
+    return (
+        <ChatContext.Provider value={{ username, messages, sendMessage }}>
+        {children}
+        </ChatContext.Provider>
+    );
+}
+
+export const useChat = () => useContext(ChatContext);
